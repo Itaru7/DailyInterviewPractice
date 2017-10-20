fd12e5029640835dad73deb5378bd5b06d340d14
Itaru7 <ir172178@gmail.com>
2017-10-20 02:04:13 -0700

    Added the correct code

---
+++ 2017/10. October/10-19-stack-and-queue/stack-and-queue.py
@@ -0,0 +1,17 @@
+import math
+
+def print_formatted(number):
+    # your code goes here
+    i = 0
+    width = int(math.log(number, 2)) + 2
+    first = width - 1
+    while i < number:
+        i += 1
+        o = '{0:o}'.format(i)
+        h = '{0:X}'.format(i)
+        b = '{0:b}'.format(i)
+        print('{0:>{5}}{1:>{4}}{2:>{4}}{3:>{4}}'.format(i, o, h, b, width, first))
+
+if __name__ == '__main__':
+    n = int(input('Please Enter Number>>'))
+    print_formatted(n)
